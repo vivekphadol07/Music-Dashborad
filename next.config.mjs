d66aa049d83cbc19f59a2040f2572f4dec77835e
vivekphadol07 <vivekphadol31@gmail.com>
2025-09-23 11:52:47 +0530

    Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/Music-Dashborad',  
+  reactStrictMode: true,  
 };
 
 export default nextConfig;
