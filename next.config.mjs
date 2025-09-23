1a06235d29092b1c11a5bc7b458be11aa76b48ae
Vivek Phadol <vivekphadol31@gmail.com>
2025-09-23 10:38:58 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    "output":'export',
+    reactStrictMode: true,
+  basePath: "/Music-Dashboard",  
+  assetPrefix: "/Music-Dashboard/",
 };
 
 export default nextConfig;
