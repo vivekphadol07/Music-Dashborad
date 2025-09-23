27631c1ea9d7b05b202f936ea2d900254530b869
vivekphadol07 <vivekphadol31@gmail.com>
2025-09-23 11:13:40 +0530

    Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  basePath: "/Music-Dashboard",  
+  output: "export",
+  basePath: "/Music-Dashboard",
   assetPrefix: "/Music-Dashboard/",
 };
 
